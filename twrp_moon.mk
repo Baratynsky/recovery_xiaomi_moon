b61d2f01a6c0134779bb0a148e1ede21d58c9a03
Nikita <baratynsky.nikita@gmail.com>
2025-05-29 12:30:48 +0500

    Update twrp_moon.mk

--- twrp_moon.mk
+++ twrp_moon.mk
@@ -6,6 +6,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# -----------------------------------------------------------------------------
+# ВАЖНОЕ ЗАМЕЧАНИЕ:
+# Убедитесь, что ваш файл device/xiaomi/moon/BoardConfig.mk
+# корректен и специфичен для Xiaomi Redmi 13 (moon), особенно
+# настройки архитектуры процессора (TARGET_CPU_VARIANT и т.д.).
+# -----------------------------------------------------------------------------
+
 # Наследование от базовых продуктов. Наиболее специфичные первыми.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
@@ -20,31 +27,49 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 
-# Убедитесь, что файл device/xiaomi/moon/device.mk существует и корректно настроен.
+# Наследование от конфигурации устройства moon
 $(call inherit-product, device/xiaomi/moon/device.mk)
 
 # Идентификация продукта
 PRODUCT_DEVICE := moon
-PRODUCT_NAME := twrp_moon
+PRODUCT_NAME := twrp_moon # Это имя будет использовано для имени сборки, например, twrp_moon.zip
 PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := Redmi 13
+PRODUCT_MODEL := Redmi 13 # Это рыночное имя, которое будет отображаться
 PRODUCT_MANUFACTURER := Xiaomi
 
 # GMS client ID для устройств Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+# Переопределение свойств системы для TWRP.
+# Цель - чтобы TWRP "представлялся" системе как можно ближе к стоковой прошивке moon_ru.
 PRODUCT_PROPERTY_OVERRIDES += \
+    # --- Идентификация сборки ---
     ro.build.fingerprint=Redmi/moon_ru/moon:15/AP3A.240905.015.A2/OS2.0.5.0.VNTRUXM:user/release-keys \
     ro.build.description=moon_ru-user 15 AP3A.240905.015.A2 OS2.0.5.0.VNTRUXM release-keys \
-    ro.product.system.brand=$(PRODUCT_BRAND) \
-    ro.product.vendor.brand=$(PRODUCT_BRAND) \
-    ro.product.system.device=$(PRODUCT_DEVICE) \
-    ro.product.vendor.device=$(PRODUCT_DEVICE) \
-    ro.product.system.manufacturer=$(PRODUCT_MANUFACTURER) \
-    ro.product.vendor.manufacturer=$(PRODUCT_MANUFACTURER) \
-    ro.product.system.model=$(PRODUCT_MODEL) \
-    ro.product.vendor.model=24040RN64Y \
+    # Явно переопределяем свойства продукта, чтобы избежать конфликтов с авто-генерируемыми
+    ro.product.name=moon_ru \
+    ro.product.device=$(PRODUCT_DEVICE) \
+    ro.product.model=$(PRODUCT_MODEL) \
+    ro.product.brand=$(PRODUCT_BRAND) \
+    ro.product.manufacturer=$(PRODUCT_MANUFACTURER) \
+    # Системные свойства продукта
     ro.product.system.name=moon_ru \
+    ro.product.system.device=$(PRODUCT_DEVICE) \
+    ro.product.system.model=$(PRODUCT_MODEL) \
+    ro.product.system.brand=$(PRODUCT_BRAND) \
+    ro.product.system.manufacturer=$(PRODUCT_MANUFACTURER) \
+    # Вендорные свойства продукта
     ro.product.vendor.name=moon_ru \
+    ro.product.vendor.device=$(PRODUCT_DEVICE) \
+    ro.product.vendor.model=24040RN64Y \
+    ro.product.vendor.brand=$(PRODUCT_BRAND) \
+    ro.product.vendor.manufacturer=$(PRODUCT_MANUFACTURER) \
+    # Рыночные имена
     ro.product.marketname=$(PRODUCT_MODEL) \
     ro.vendor.product.marketname=$(PRODUCT_MODEL)
+
+# Если вам нужно, чтобы TWRP явно устанавливал аппаратный SKU (хотя обычно это не требуется,
+# так как TWRP его читает из bootloader):
+# PRODUCT_PROPERTY_OVERRIDES += \
+#     ro.boot.product.hardware.sku=moon_gl
+
